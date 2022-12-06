fb500ebc92235e864b8897761b2156c553a5824a
Zakharievich <lerugoff@yandex.ru>
2022-12-06 22:39:19 +0300

    Update def __main__ in file function.py

--- function.py
+++ function.py
@@ -93,20 +93,13 @@ def calc_e(x):
     return [(math.e)**(-2*x)]
 
 
-def show_result(x2, cos, e):
+def main(x2, cos, e):
     if (len(x2) > 0) and (len(cos) > 0) and (len(e) > 0):
         f = x2[0] + cos[0]*e[0]
         return f
     else:
         return 'Error calc function'
 
-
-def main():
-    x = float(input('Enter Х: '))
-    res_x2 = calc_x2(x)
-    res_cos = calc_cos(x)
-    res_e = calc_e(x)
-    show_result(res_x2, res_cos, res_e)
 
 if __name__ == '__main__':
     main()
